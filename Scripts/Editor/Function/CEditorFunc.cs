6c538202824a5efdc523e5ae2f9d478045366dda
sd.lee <sd.lee@ninetap.com>
2020-10-14 22:47:59 +0900

    [이상동] 에디터 경과 창 출력 로직 수정

--- Scripts/Editor/Function/CEditorFunc.cs
+++ Scripts/Editor/Function/CEditorFunc.cs
@@ -17,7 +17,8 @@ public static partial class CEditorFunc {
 
 	//! 경고 팝업을 출력한다
 	public static bool ShowAlertPopup(string a_oTitle,
-		string a_oMsg, string a_oOKBtnText, string a_oCancelBtnText) {
+		string a_oMsg, string a_oOKBtnText, string a_oCancelBtnText = KCDefine.B_EMPTY_STRING) 
+	{
 		// 취소 버튼 텍스트가 유효 할 경우
 		if(a_oCancelBtnText.ExIsValid()) {
 			return EditorUtility.DisplayDialog(a_oTitle, a_oMsg, a_oOKBtnText, a_oCancelBtnText);
