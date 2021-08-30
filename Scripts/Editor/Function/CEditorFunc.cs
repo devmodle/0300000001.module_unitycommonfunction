90a5495cfa52b96f5a1a70270e34f044f6ad7e80
dante <are2341@nate.com>
2021-08-31 04:33:41 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Editor/Function/CEditorFunc.cs
+++ Scripts/Editor/Function/CEditorFunc.cs
@@ -54,8 +54,8 @@ public static partial class CEditorFunc {
 
 	//! 커맨드 라인을 실행한다
 	public static void ExecuteCmdLine(string a_oFilePath, string a_oParams, bool a_bIsEnableAssert = true) {
-		CAccess.Assert(!a_bIsEnableAssert || (a_oFilePath.ExIsValid() && a_oParams.ExIsValid()));
 		CFunc.ShowLog($"CEditorFunc.ExecuteCmdLine: {a_oFilePath}, {a_oParams}");
+		CAccess.Assert(!a_bIsEnableAssert || (a_oFilePath.ExIsValid() && a_oParams.ExIsValid()));
 
 		// 커맨드 라인 실행이 가능 할 경우
 		if(a_oFilePath.ExIsValid() && a_oParams.ExIsValid()) {
