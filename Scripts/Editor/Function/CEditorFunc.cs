285c7361c9a7ef42f457da6b6188ac759101b5e6
dante <are2341@nate.com>
2021-09-10 01:56:43 +0900

    [이상동] 쉘 명령어 수정

--- Scripts/Editor/Function/CEditorFunc.cs
+++ Scripts/Editor/Function/CEditorFunc.cs
@@ -43,7 +43,8 @@ public static partial class CEditorFunc {
 		if(a_oParams.ExIsValid()) {
 			// 맥 일 경우
 			if(CAccess.IsMac) {
-				CEditorFunc.ExecuteCmdLine(KCEditorDefine.B_TOOL_P_SHELL, string.Format(KCEditorDefine.B_CMD_LINE_PARAMS_FMT_SHELL, a_oParams), a_bIsAsync, a_bIsEnableAssert);
+				string oParams = string.Format(KCDefine.B_TEXT_FMT_2_SEMI_COLON_COMBINE, KCEditorDefine.B_BUILD_CMD_EXPORT_PATH, a_oParams);
+				CEditorFunc.ExecuteCmdLine(KCEditorDefine.B_TOOL_P_SHELL, string.Format(KCEditorDefine.B_CMD_LINE_PARAMS_FMT_SHELL, oParams), a_bIsAsync, a_bIsEnableAssert);
 			}
 			// 윈도우즈 일 경우
 			else if(CAccess.IsWnds) {
