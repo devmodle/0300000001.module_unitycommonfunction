9180402b5826cd857397456bfc36c92bf381d100
dante <are2341@nate.com>
2022-02-04 19:24:53 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Runtime/Function/CFunc+Setup.cs
+++ Scripts/Runtime/Function/CFunc+Setup.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
@@ -8,34 +8,75 @@ using UnityEngine.Rendering;
 public static partial class CFunc {
 	#region 클래스 함수
 	/** 퀄리티를 설정한다 */
-	public static void SetupQuality(EQualityLevel a_eQualityLevel, RenderPipelineAsset a_oRenderingPipeline, bool a_bIsEnableExpensiveChange = false) {
+	public static void SetupQuality(EQualityLevel a_eQualityLevel, bool a_bIsEnableSetupRenderingPipeline, bool a_bIsEnableExpensiveChange = false) {
 #if UNITY_EDITOR
-		for(int i = (int)EQualityLevel.VERY_LOW; i < (int)EQualityLevel.MAX_VAL; ++i) {
-			QualitySettings.SetQualityLevel(i, false);
-			QualitySettings.renderPipeline = a_oRenderingPipeline;
+		var oQualityLevelList = new List<EQualityLevel>() {
+			EQualityLevel.NORM, EQualityLevel.HIGH, EQualityLevel.ULTRA
+		};
 
+		for(int i = 0; i < oQualityLevelList.Count; ++i) {
+			QualitySettings.SetQualityLevel((int)oQualityLevelList[i], false);
+			QualitySettings.renderPipeline = a_bIsEnableSetupRenderingPipeline ? Resources.Load<RenderPipelineAsset>(CAccess.GetRenderingPipelinePath(oQualityLevelList[i])) : null;
+
+			QualitySettings.softParticles = true;
 			QualitySettings.streamingMipmapsActive = false;
 			QualitySettings.asyncUploadPersistentBuffer = true;
 			QualitySettings.billboardsFaceCameraPosition = true;
 
 			QualitySettings.lodBias = KCDefine.B_VAL_1_FLT;
-			QualitySettings.antiAliasing = KCDefine.B_VAL_0_INT;
-			QualitySettings.maximumLODLevel = KCDefine.B_VAL_3_INT;
+			QualitySettings.maximumLODLevel = KCDefine.B_VAL_0_INT;
 			QualitySettings.resolutionScalingFixedDPIFactor = KCDefine.B_VAL_1_FLT;
 
 			QualitySettings.asyncUploadTimeSlice = KCDefine.U_QUALITY_ASYNC_UPLOAD_TIME_SLICE;
 			QualitySettings.asyncUploadBufferSize = KCDefine.U_QUALITY_ASYNC_UPLOAD_BUFFER_SIZE;
 
+			QualitySettings.vSyncCount = (int)EVSyncType.NEVER;
+			QualitySettings.skinWeights = SkinWeights.FourBones;
+			QualitySettings.shadowCascades = (int)EShadowCascadesOpts.FOUR_CASCADES;
+			QualitySettings.shadowProjection = ShadowProjection.StableFit;
+			QualitySettings.anisotropicFiltering = AnisotropicFiltering.Enable;
+
 			QualitySettings.shadowDistance = KCDefine.U_DISTANCE_CAMERA_FAR_PLANE / KCDefine.B_VAL_5_FLT;
 			QualitySettings.shadowCascade2Split = KCEditorDefine.U_EDITOR_OPTS_CASCADE_2_SPLIT_PERCENT;
 			QualitySettings.shadowCascade4Split = KCEditorDefine.U_EDITOR_OPTS_CASCADE_4_SPLIT_PERCENT;
+			QualitySettings.shadowNearPlaneOffset = KCDefine.U_DISTANCE_CAMERA_NEAR_PLANE;
 
+			switch(oQualityLevelList[i]) {
+				case EQualityLevel.HIGH: {
+					QualitySettings.shadowmaskMode = ShadowmaskMode.DistanceShadowmask;
+					QualitySettings.shadowResolution = ShadowResolution.High;
+				} break;
+				case EQualityLevel.ULTRA: {
+					QualitySettings.shadowmaskMode = ShadowmaskMode.DistanceShadowmask;
+					QualitySettings.shadowResolution = ShadowResolution.VeryHigh;
+				} break;
+				default: {
+					QualitySettings.shadowmaskMode = ShadowmaskMode.Shadowmask;
+					QualitySettings.shadowResolution = ShadowResolution.Medium;
+				} break;
+			}
+
+#if MSAA_ENABLE
+			switch(oQualityLevelList[i]) {
+				case EQualityLevel.HIGH: {
+					QualitySettings.antiAliasing = (int)EAntiAliasingLevel.FOUR_SAMPLE;
+				} break;
+				case EQualityLevel.ULTRA: {
+					QualitySettings.antiAliasing = (int)EAntiAliasingLevel.EIGHT_SAMPLE;
+				} break;
+				default: {
+					QualitySettings.antiAliasing = (int)EAntiAliasingLevel.TWO_SAMPLE;
+				} break;
+			}
+#else
+			QualitySettings.antiAliasing = (int)EAntiAliasingLevel.DISABLE;
+#endif			// #if MSAA_ENABLE
+
+#if LIGHT_ENABLE
 			QualitySettings.shadows = ShadowQuality.All;
-			QualitySettings.vSyncCount = (int)EVSyncType.NEVER;
-			QualitySettings.skinWeights = ((EQualityLevel)i >= EQualityLevel.HIGH) ? SkinWeights.FourBones : SkinWeights.TwoBones;
-			QualitySettings.shadowmaskMode = ((EQualityLevel)i >= EQualityLevel.HIGH) ? ShadowmaskMode.DistanceShadowmask : ShadowmaskMode.Shadowmask;
-			QualitySettings.shadowCascades = ((EQualityLevel)i >= EQualityLevel.HIGH) ? (int)EShadowCascadesOpts.FOUR_CASCADES : (int)EShadowCascadesOpts.TWO_CASCADES;
-			QualitySettings.anisotropicFiltering = ((EQualityLevel)i >= EQualityLevel.HIGH) ? AnisotropicFiltering.Enable : AnisotropicFiltering.Disable;
+#else
+			QualitySettings.shadows = ShadowQuality.Disable;
+#endif			// #if LIGHT_ENABLE
 
 #if REALTIME_REFLECTION_PROBES_ENABLE
 			QualitySettings.realtimeReflectionProbes = true;
@@ -43,14 +84,15 @@ public static partial class CFunc {
 			QualitySettings.realtimeReflectionProbes = false;
 #endif			// #if REALTIME_REFLECTION_PROBES_ENABLE
 		}
-
+		
+		GraphicsSettings.logWhenShaderIsCompiled = true;
 		GraphicsSettings.videoShadersIncludeMode = VideoShadersIncludeMode.Always;
 #endif			// #if UNITY_EDITOR
 
-		GraphicsSettings.renderPipelineAsset = a_oRenderingPipeline;
-		GraphicsSettings.defaultRenderPipeline = a_oRenderingPipeline;
+		GraphicsSettings.renderPipelineAsset = a_bIsEnableSetupRenderingPipeline ? Resources.Load<RenderPipelineAsset>(CAccess.GetRenderingPipelinePath(a_eQualityLevel)) : null;
+		GraphicsSettings.defaultRenderPipeline = a_bIsEnableSetupRenderingPipeline ? Resources.Load<RenderPipelineAsset>(CAccess.GetRenderingPipelinePath(a_eQualityLevel)) : null;
 
-		QualitySettings.SetQualityLevel((int)((a_eQualityLevel == EQualityLevel.AUTO) ? CAccess.AutoQualityLevel : a_eQualityLevel), a_bIsEnableExpensiveChange);
+		QualitySettings.SetQualityLevel((int)a_eQualityLevel, a_bIsEnableExpensiveChange);
 	}
 	
 	/** 화면 UI 를 설정한다 */
