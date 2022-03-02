b910322db0d4a4bf0d2582f0b04e7c59523d13e6
sd.lee <sd.lee@ninetap.com>
2022-03-03 08:53:59 +0900

    [이상동] 서브 모듈 갱신

--- Scripts/Runtime/Function/CFunc+Utility.cs
+++ Scripts/Runtime/Function/CFunc+Utility.cs
@@ -123,21 +123,21 @@ public static partial class CFunc {
 #if UNITY_ANDROID
 			// 권한이 유효 할 경우
 			if(CAccess.IsEnablePermission(a_oPermission)) {
-				a_oCallback?.Invoke(a_oPermission, true);
+				CFunc.Invoke(ref a_oCallback, a_oPermission, true);
 			} else {
 				Permission.RequestUserPermission(a_oPermission);
 				
 				a_oComponent.ExRepeatCallFunc((a_oSender, a_bIsComplete) => {
 					// 완료 되었을 경우
 					if(a_bIsComplete) {
-						a_oCallback?.Invoke(a_oPermission, CAccess.IsEnablePermission(a_oPermission));
+						CFunc.Invoke(ref a_oCallback, a_oPermission, CAccess.IsEnablePermission(a_oPermission));
 					}
 
 					return !CAccess.IsEnablePermission(a_oPermission);
 				}, KCDefine.U_DELTA_T_PERMISSION_M_REQUEST_CHECK, KCDefine.U_MAX_DELTA_T_PERMISSION_M_REQUEST_CHECK, a_bIsRealtime);
 			}
 #else
-			a_oCallback?.Invoke(a_oPermission, false);
+			CFunc.Invoke(ref a_oCallback, a_oPermission, false);
 #endif			// #if UNITY_ANDROID
 		}
 	}
