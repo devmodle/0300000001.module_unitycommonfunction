07e1db5b8f289cdb16793d9cddaaaa5a32702743
lkstudio.dante <lkstudio.dante@gmail.com>
2024-02-01 03:58:04 +0900

    [이상동] 서브 모듈 갱신

--- Client/Scripts/Editor/Function/CEditorFunc+Popup.cs
+++ Client/Scripts/Editor/Function/CEditorFunc+Popup.cs
@@ -12,7 +12,7 @@ public static partial class CEditorFunc {
 	#region 클래스 함수
 	/** 경고 팝업을 출력한다 */
 	public static bool ShowOKAlertPopup(string a_oTitle, string a_oMsg) {
-		return CEditorFunc.ShowAlertPopup(a_oTitle, a_oMsg, KCEditorDefine.B_TEXT_OK);
+		return CEditorFunc.ShowAlertPopup(a_oTitle, a_oMsg, KCEditorDefine.B_TEXT_OK, string.Empty);
 	}
 
 	/** 경고 팝업을 출력한다 */
@@ -29,10 +29,10 @@ public static partial class CEditorFunc {
 	private static bool ShowAlertPopup(string a_oTitle, string a_oMsg, string a_oOKBtnText, string a_oCancelBtnText) {
 		// 취소 버튼 텍스트가 유효 할 경우
 		if(a_oCancelBtnText.ExIsValid()) {
-			return EditorUtility.DisplayDialog(a_oTitle, a_oMsg, oOKBtnText, a_oCancelBtnText);
+			return EditorUtility.DisplayDialog(a_oTitle, a_oMsg, a_oOKBtnText, a_oCancelBtnText);
 		}
 
-		return EditorUtility.DisplayDialog(a_oTitle, a_oMsg, oOKBtnText);
+		return EditorUtility.DisplayDialog(a_oTitle, a_oMsg, a_oOKBtnText);
 	}
 	#endregion // 클래스 함수
 }
